a17cca6823fcef8e09fe950d0c78f2799bad5dcc
Jakub Wróbel <83750224+jakubwrobel2003@users.noreply.github.com>
2025-04-23 21:31:15 +0200

    BH
    BH

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,26 @@
-﻿using System.Net.NetworkInformation;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Generyki;
+
 namespace WpfApp5
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private Dictionary<string, Student> studenci;
         private Regał<string> regał = new Regał<string>();
+        private Queue<Zadanie> kolejka = new Queue<Zadanie>();
 
         public MainWindow()
         {
             InitializeComponent();
             AktualizujPółki();
             studenci = new Dictionary<string, Student>{
-                {"1",new Student("MESSI",5.0) },
-                {"2",new Student("GAVI",5.0) },
-                {"3",new Student("PEDRI",5.0) },
-                {"4",new Student("BALDE",5.0) },
-                {"5",new Student("VINI",5.0) }
+                {"1",new Student("MESSI",5.0)},
+                {"2",new Student("GAVI",5.0)},
+                {"3",new Student("PEDRI",5.0)},
+                {"4",new Student("BALDE",5.0)}
             };
         }
 
@@ -41,139 +31,316 @@ namespace WpfApp5
             txtPółka3.Text = $"Półka3: {regał.Półka3}";
         }
 
-     
         private void OnWstawNaWolnąPółkęClick(object sender, RoutedEventArgs e)
         {
-            string wartość = inputTextBox.Text;
-            regał.WstawNaWolnąPółkę(wartość);
+            regał.WstawNaWolnąPółkę(inputTextBox.Text);
             AktualizujPółki();
         }
 
-        
         private void OnWstawNaPółkę1Click(object sender, RoutedEventArgs e)
         {
-            string wartość = inputTextBox.Text;
-            regał.Półka1 = wartość;
+            regał.Półka1 = inputTextBox.Text;
             AktualizujPółki();
         }
 
-        
         private void OnWstawNaPółkę2Click(object sender, RoutedEventArgs e)
         {
-            string wartość = inputTextBox.Text;
-            regał.Półka2 = wartość;
+            regał.Półka2 = inputTextBox.Text;
             AktualizujPółki();
         }
 
         private void OnWstawNaPółkę3Click(object sender, RoutedEventArgs e)
         {
-            string wartość = inputTextBox.Text;
-            regał.Półka3 = wartość;
+            regał.Półka3 = inputTextBox.Text;
             AktualizujPółki();
         }
-    
 
-    private void btnSzukaj_Click(object sender, RoutedEventArgs e)
+        private void btnSzukaj_Click(object sender, RoutedEventArgs e)
         {
-            string numerAlbumu = txtNumerAlbumu.Text;
-
-            if (studenci.ContainsKey(numerAlbumu))
+            string nr = txtNumerAlbumu.Text;
+            if (studenci.TryGetValue(nr, out Student student))
             {
-                Student znalezionyStudent = studenci[numerAlbumu];
-                MessageBox.Show($"Student: {znalezionyStudent.Nazwisko}\nOcena: {znalezionyStudent.Ocena}",
-                                "Znaleziony student", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"Student: {student.Nazwisko}, Ocena: {student.Ocena}");
             }
             else
             {
-                MessageBox.Show("Nie znaleziono studenta o podanym numerze albumu.",
-                                "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Nie znaleziono studenta");
             }
         }
+
         private void btnCreateStozek_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 Stozek stozek = new Stozek();
-
                 stozek.OBSLUGA += Stozek_OBSLUGA;
 
-                double wysokosc = Convert.ToDouble(txtWysokosc.Text);
-                double promien = Convert.ToDouble(txtPromien.Text);
-
-                stozek.H = wysokosc;
-                stozek.Promien = promien;
+                stozek.H = double.Parse(txtWysokosc.Text);
+                stozek.Promien = double.Parse(txtPromien.Text);
             }
-            catch (FormatException)
+            catch
             {
-                MessageBox.Show("Wprowadź poprawne liczby w polach wysokości i promienia.");
+                MessageBox.Show("Podaj liczby!");
             }
         }
-
 
         private void Stozek_OBSLUGA(string opisBledu)
         {
-
-            MessageBox.Show(opisBledu, "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-
-
+            MessageBox.Show(opisBledu, "Błąd");
             lblError.Content = opisBledu;
         }
+
         private void TestZnajdzWiekszy_Click(object sender, RoutedEventArgs e)
         {
-            string a1 = "ALL";
-            string a2 = "BLL";
-            string wynik1 = Test.ZnajdzWiekszy(a1, a2);
-            MessageBox.Show("Większy string: " + wynik1);
-            double b1 = 3;
-            double b2 = 4;
-            double wynik2 = Test.ZnajdzWiekszy(b1, b2);
-            MessageBox.Show("Większy double " + wynik2);
+            MessageBox.Show("String: " + Test.ZnajdzWiekszy("AAA", "BBB"));
+            MessageBox.Show("Double: " + Test.ZnajdzWiekszy(3.5, 7.2));
 
-            Student s1 = new Student("MESSI", 5.0);
-           Student s2 = new Student("MESSI", 5.0);
-
-            Student s3 = Test.ZnajdzWiekszy(s1, s2);
-            MessageBox.Show("Lepszy student: " + s3);
+            Student s1 = new Student("Lewy", 4.5);
+            Student s2 = new Student("Messi", 5.0);
+            MessageBox.Show("Student: " + Test.ZnajdzWiekszy(s1, s2));
         }
-    }
 
+        private void btnTestSamochod_Click(object sender, RoutedEventArgs e)
+        {
+            var auto = new Samochód();
+            ((IZwiększany)auto).Zmień();
+            listBox.Items.Add(auto.ToString());
+            ((IZmniejszany)auto).Zmień();
+            listBox.Items.Add(auto.ToString());
+        }
+
+        private void btnPopraw_Click(object sender, RoutedEventArgs e)
+        {
+            var student = new Student("Pedri", 4.0);
+            listBox.Items.Add("Przed: " + student);
+            var lepszy = student.PobierzLepsząWersję();
+            listBox.Items.Add("Po: " + lepszy);
+        }
+
+        private void DodajZadanie_Click(object sender, RoutedEventArgs e)
+        {
+            string opis = Microsoft.VisualBasic.Interaction.InputBox("Wpisz opis zadania:", "Dodaj zadanie");
+            if (!string.IsNullOrWhiteSpace(opis))
+            {
+                kolejka.Enqueue(new Zadanie(opis));
+            }
+        }
+
+        private void PobierzZadanie_Click(object sender, RoutedEventArgs e)
+        {
+            if (kolejka.Count > 0)
+            {
+                var z = kolejka.Dequeue();
+                MessageBox.Show(z.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Brak zadań w kolejce.");
+            }
+        }
+        private void btnWyswietlStos_Click(object sender, RoutedEventArgs e)
+        {
+            Stack<IWyświetlana> stos = new Stack<IWyświetlana>();
+            stos.Push(new Towar("Komputer", "Elektronika"));
+            stos.Push(new Towar("Chleb", "Spożywcze"));
+            stos.Push(new Towar("Koszulka", "Odzież"));
+            stos.Push(new Mieszkanie("Ul. Długa 1", 3));
+            stos.Push(new Mieszkanie("Ul. Krótka 5", 2));
+            stos.Push(new Mieszkanie("Rynek 10", 4));
+
+            listBox.Items.Clear();
+            foreach (var item in stos)
+            {
+                item.WyświetlDane(listBox);
+            }
+        }
+        private void btnStudentPolitechniki_Click(object sender, RoutedEventArgs e)
+        {
+            StudentPolitechniki student = new StudentPolitechniki("99010112345", "123456");
+
+            string pesel = ((IObywatel)student).Identyfikator;
+            string album = ((IStudent)student).Identyfikator;
+
+            lblPesel.Content = "PESEL: " + pesel;
+            lblAlbum.Content = "Nr albumu: " + album;
+        }
+        private void btnPudełkoTest_Click(object sender, RoutedEventArgs e)
+        {
+            Pudełko<string> pudelko = new Pudełko<string>(10);
+
+            pudelko.Włóż("Jabłko", 0);
+            pudelko.Włóż("Gruszka", 1);
+            pudelko.Włóż("Banan", 2);
+
+            listBox.Items.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                listBox.Items.Add(pudelko.Wyciągnij(i));
+            }
+        }
+        private void btnJedenZTrzechString_Click(object sender, RoutedEventArgs e)
+        {
+            string wynik = Narzędzia.JedenZTrzech("Ala", "Ola", "Ela");
+            MessageBox.Show("Wylosowano: " + wynik);
+        }
+
+        private void btnJedenZTrzechArmata_Click(object sender, RoutedEventArgs e)
+        {
+            Armata a1 = new Armata(120, 1000);
+            Armata a2 = new Armata(90, 800);
+            Armata a3 = new Armata(150, 1500);
+
+            Armata wynik = Narzędzia.JedenZTrzech(a1, a2, a3);
+            MessageBox.Show("Wylosowano: " + wynik);
+        }
+
+    }
 
     public class Stozek
     {
-        public delegate void ObslugaBledu(string OPIS);
-
+        public delegate void ObslugaBledu(string opis);
         public event ObslugaBledu? OBSLUGA;
-
 
         private double h;
         public double H
         {
             set
             {
-                h = value;
-                if (h < 0 && OBSLUGA != null)
-                {
-                    OBSLUGA("ZLE");
-                }
-
-
+                if (value < 0)
+                    OBSLUGA?.Invoke("Wysokość nie może być ujemna.");
+                else
+                    h = value;
             }
         }
+
         private double p;
         public double Promien
         {
             set
             {
-                p = value;
-                if (p < 0 && OBSLUGA != null)
-                {
-                    OBSLUGA("ZLE");
-                }
-
-
-
+                if (value < 0)
+                    OBSLUGA?.Invoke("Promień nie może być ujemny.");
+                else
+                    p = value;
             }
         }
-
     }
+    public interface IWyświetlana
+    {
+        void WyświetlDane(ListBox listBox);
+    }
+
+    public class Towar : IWyświetlana
+    {
+        public string Nazwa { get; set; }
+        public string Typ { get; set; }
+
+        public Towar(string nazwa, string typ)
+        {
+            Nazwa = nazwa;
+            Typ = typ;
+        }
+
+        public void WyświetlDane(ListBox listBox)
+        {
+            listBox.Items.Add($"Towar: {Nazwa}, Typ: {Typ}");
+        }
+    }
+
+    public class Mieszkanie : IWyświetlana
+    {
+        public string Adres { get; set; }
+        public int LiczbaPokoi { get; set; }
+
+        public Mieszkanie(string adres, int liczbaPokoi)
+        {
+            Adres = adres;
+            LiczbaPokoi = liczbaPokoi;
+        }
+
+        public void WyświetlDane(ListBox listBox)
+        {
+            listBox.Items.Add($"Mieszkanie: {Adres}, Pokoje: {LiczbaPokoi}");
+        }
+    }
+    public interface IObywatel
+    {
+        string Identyfikator { get; }
+    }
+
+    public interface IStudent
+    {
+        string Identyfikator { get; }
+    }
+
+    public class StudentPolitechniki : IObywatel, IStudent
+    {
+        private string pesel;
+        private string nrAlbumu;
+
+        public StudentPolitechniki(string pesel, string nrAlbumu)
+        {
+            this.pesel = pesel;
+            this.nrAlbumu = nrAlbumu;
+        }
+
+        string IObywatel.Identyfikator => pesel;
+        string IStudent.Identyfikator => nrAlbumu;
+    }
+    public class Pudełko<T>
+    {
+        private T[] przegrody;
+
+        public Pudełko(int rozmiar)
+        {
+            przegrody = new T[rozmiar];
+        }
+
+        public void Włóż(T element, int nrPrzegrody)
+        {
+            if (nrPrzegrody < 0 || nrPrzegrody >= przegrody.Length)
+                throw new ArgumentException("Numer przegrody poza zakresem.");
+
+            przegrody[nrPrzegrody] = element;
+        }
+
+        public T Wyciągnij(int nrPrzegrody)
+        {
+            if (nrPrzegrody < 0 || nrPrzegrody >= przegrody.Length)
+                throw new ArgumentException("Numer przegrody poza zakresem.");
+
+            return przegrody[nrPrzegrody];
+        }
+    }
+    public static class Narzędzia
+    {
+        public static T JedenZTrzech<T>(T a, T b, T c)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, 3);
+            return i switch
+            {
+                0 => a,
+                1 => b,
+                _ => c,
+            };
+        }
+    }
+    public class Armata
+    {
+        public double Kaliber { get; set; }
+        public double Masa { get; set; }
+
+        public Armata(double kaliber, double masa)
+        {
+            Kaliber = kaliber;
+            Masa = masa;
+        }
+
+        public override string ToString()
+        {
+            return $"Kaliber: {Kaliber}, Masa: {Masa}kg";
+        }
+    }
+
 }
--- Student.cs
+++ Student.cs
@@ -1,6 +1,14 @@
-﻿namespace Generyki
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generyki
 {
-    public class Student : IComparable<Student>
+    public interface IPoprawialny<T>
+    {
+        T PobierzLepsząWersję();
+    }
+
+    public class Student : IComparable<Student>, IPoprawialny<Student>
     {
         public string Nazwisko { get; set; }
         public double Ocena { get; set; }
@@ -10,60 +18,101 @@
             Nazwisko = nazwisko;
             Ocena = ocena;
         }
+
         public int CompareTo(Student? other)
         {
             if (other == null) return 1;
             return this.Ocena.CompareTo(other.Ocena);
         }
 
+        public Student PobierzLepsząWersję()
+        {
+            return new Student(Nazwisko, Math.Min(Ocena + 0.5, 5.0));
+        }
+
         public override string ToString()
         {
-            return $"STUDent {Nazwisko}, OCena {Ocena}";
+            return $"Student: {Nazwisko}, Ocena: {Ocena}";
         }
     }
+
     public static class Test
     {
         public static T ZnajdzWiekszy<T>(T a, T b) where T : IComparable<T>
         {
-            
-            return a.CompareTo(b) > 0 ? a : b;
+            return a.CompareTo(b) >= 0 ? a : b;
         }
     }
 
     public class Regał<T>
     {
-
         public T Półka1 { get; set; } = default(T);
         public T Półka2 { get; set; } = default(T);
         public T Półka3 { get; set; } = default(T);
-
 
         public override string ToString()
         {
             return $"Półka1: {Półka1}, Półka2: {Półka2}, Półka3: {Półka3}";
         }
 
-
         public void WstawNaWolnąPółkę(T wartość)
         {
-            if (Półka1.Equals(default(T)))
-            {
+            if (EqualityComparer<T>.Default.Equals(Półka1, default(T)))
                 Półka1 = wartość;
-            }
-            else if (Półka2.Equals(default(T)))
-            {
+            else if (EqualityComparer<T>.Default.Equals(Półka2, default(T)))
                 Półka2 = wartość;
-            }
-            else if (Półka3.Equals(default(T)))
-            {
+            else if (EqualityComparer<T>.Default.Equals(Półka3, default(T)))
                 Półka3 = wartość;
-            }
-           
         }
 
-          public T WolnaPółka
+        public T WolnaPółka
         {
             set { WstawNaWolnąPółkę(value); }
         }
+    }
+
+    public class Zadanie
+    {
+        public string Opis { get; set; }
+        public DateTime DataWprowadzenia { get; set; }
+
+        public Zadanie(string opis)
+        {
+            Opis = opis;
+            DataWprowadzenia = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Opis} (Dodano: {DataWprowadzenia})";
+        }
+    }
+
+    public interface IZwiększany
+    {
+        void Zmień();
+    }
+
+    public interface IZmniejszany
+    {
+        void Zmień();
+    }
+
+    public class Samochód : IZwiększany, IZmniejszany, IPoprawialny<Samochód>
+    {
+        public int Prędkość { get; private set; } = 0;
+
+        void IZwiększany.Zmień() => Prędkość += 10;
+        void IZmniejszany.Zmień() => Prędkość -= 5;
+
+        public Samochód PobierzLepsząWersję()
+        {
+            var kopia = new Samochód();
+            ((IZwiększany)kopia).Zmień();
+            ((IZwiększany)kopia).Zmień();
+            return kopia;
+        }
+
+        public override string ToString() => $"Prędkość: {Prędkość} km/h";
     }
 }
